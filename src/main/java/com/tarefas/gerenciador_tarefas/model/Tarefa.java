d11706fc785b2fc4f5f38b2a5fab51ef1a366434
Vlade <nekaytsu@gmail.com>
2025-05-21 22:12:24 -0300

    feat: adicionando entidade Tarefa com campos id, titulo, descricao e status

---
+++ src/main/java/com/tarefas/gerenciador_tarefas/model/Tarefa.java
@@ -0,0 +1,58 @@
+package com.tarefas.gerenciador_tarefas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private String status;
+
+ 
+    public Tarefa() {}
+
+
+    public Tarefa(String titulo, String descricao, String status) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
